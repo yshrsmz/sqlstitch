2694f91cc1ede94b9cec9cdee49f8dce4d52cd53
yshrsmz <the.phantom.bane@gmail.com>
2024-01-28 19:35:11 +0900

    refactor: separate logic from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,12 @@
+mod sqlstitch;
+
 use std::{
     env,
     fs::File,
     io::{self, Read},
 };
 
-use petgraph::{graph::DiGraph, algo::toposort};
-use sqlparser::{ast::Statement, dialect, parser::Parser};
-
-#[derive(Clone, Debug)]
-struct RelatedTables {
-    source: String,
-    name: String,
-    related_tables: Vec<String>,
-}
-
-fn extract_foreign_key_constraints(sql: String) -> RelatedTables {
-    let dialect = dialect::GenericDialect {};
-    let ast = Parser::parse_sql(&dialect, &sql).unwrap();
-
-    let mut related_tables = Vec::new();
-    let mut name =String::new();
-
-    for statement in ast {
-        if let Statement::CreateTable {
-            name: table_name, constraints, ..
-        } = statement
-        {
-            name = table_name.to_string();
-            for constraint in constraints {
-                if let sqlparser::ast::TableConstraint::ForeignKey { foreign_table, .. } =
-                    constraint
-                {
-                    related_tables.push(foreign_table.to_string());
-                }
-            }
-        }
-    }
-
-    RelatedTables {
-        source: sql,
-        name,
-        related_tables,
-    }
-}
+use sqlstitch::{extract_foreign_key_constraints, sort_tables_by_foreign_key_constraints, RelatedTables};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -67,23 +31,7 @@ fn main() -> io::Result<()> {
         tables.push(result);
     }
 
-    let mut graph = DiGraph::<_, ()>::new();
-    let mut indices = std::collections::HashMap::new();
-
-    for table in &tables {
-        indices.insert(table.name.clone(), graph.add_node(table.name.clone()));
-    }
-
-    for table in &tables {
-        let source_index = *indices.get(&table.name).unwrap();
-        for related_table in &table.related_tables {
-            let target_index = *indices.get(related_table).unwrap();
-            graph.add_edge(source_index, target_index, ());
-        }
-    }
-
-    let sorted_indices = toposort(&graph, None).unwrap();
-    let sorted_tables: Vec<_> = sorted_indices.iter().rev().map(|i| graph[*i].clone()).collect();
+    let sorted_tables = sort_tables_by_foreign_key_constraints(&tables);
 
     for table in sorted_tables {
         let result = tables.iter().find(|t| t.name == table).unwrap();
